6a02322acade2d72ddfcf5d4e9a95eb74a116fc0
Jason Bock <jason@jasonbock.net>
2019-10-07 07:51:35 -0500

    Added streaming gRPC

--- Collatz.Client/Program.cs
+++ Collatz.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CollatzGrpc;
 using Google.Protobuf;
+using Grpc.Core;
 using Grpc.Net.Client;
 using System;
 using System.Linq;
@@ -17,14 +18,37 @@ namespace Collatz.Client
 
 			var channel = GrpcChannel.ForAddress("https://localhost:5501");
 			var client = new CollatzGrpc.Collatz.CollatzClient(channel);
-			var reply = await client.CalculateAsync(
-				new CollatzRequest() { Number = ByteString.CopyFrom(new BigInteger(5).ToByteArray()) });
 
-			await Console.Out.WriteLineAsync(string.Join(", ",
-				reply.Sequence.Select(_ => new BigInteger(_.ToByteArray()).ToString())));
+			await Calculate(client);
+			await CalculateWithStreaming(client);
 
 			await Console.Out.WriteLineAsync("Press enter to quit...");
 			await Console.In.ReadLineAsync();
+		}
+
+		private static async Task CalculateWithStreaming(CollatzGrpc.Collatz.CollatzClient client)
+		{
+			var request = new CollatzRequest()
+			{
+				Number = ByteString.CopyFrom(new BigInteger(55).ToByteArray())
+			};
+
+			using var responseCall = client.CalculateStream(request);
+
+			await foreach (var response in responseCall.ResponseStream.ReadAllAsync())
+			{
+				await Console.Out.WriteLineAsync(string.Join(", ",
+					response.Sequence.Select(_ => new BigInteger(_.ToByteArray()).ToString())));
+			}
+		}
+
+		private static async Task Calculate(CollatzGrpc.Collatz.CollatzClient client)
+		{
+			var reply = await client.CalculateAsync(
+				new CollatzRequest() { Number = ByteString.CopyFrom(new BigInteger(55).ToByteArray()) });
+
+			await Console.Out.WriteLineAsync(string.Join(", ",
+				reply.Sequence.Select(_ => new BigInteger(_.ToByteArray()).ToString())));
 		}
 	}
 }
--- Collatz.Host/CollatzService.cs
+++ Collatz.Host/CollatzService.cs
@@ -11,7 +11,47 @@ namespace Collatz.Service
 {
 	public sealed class CollatzService 
 		: CollatzGrpc.Collatz.CollatzBase
-	{ 
+	{
+		public override async Task CalculateStream(CollatzRequest request, 
+			IServerStreamWriter<CollatzResponse> responseStream, ServerCallContext context)
+		{
+			var random = new Random();
+			var bytes = request.Number.ToArray();
+			var start = new BigInteger(request.Number.ToArray());
+
+			if (start <= BigInteger.One)
+			{
+				throw new ArgumentException(
+					"Must provide a starting value greater than one.", nameof(start));
+			}
+
+			var sequence = new List<byte[]>
+			{
+				start.ToByteArray()
+			};
+
+			while (start > 1)
+			{
+				await Task.Delay(random.Next(250, 1000));
+				start = start % 2 == 0 ?
+					start / 2 : ((3 * start) + 1) / 2;
+				sequence.Add(start.ToByteArray());
+
+				if(sequence.Count >= 3)
+				{
+					var result = new CollatzResponse()
+					{
+						Number = ByteString.CopyFrom(start.ToByteArray()),
+					};
+
+					result.Sequence.AddRange(sequence.Select(_ => ByteString.CopyFrom(_)));
+
+					await responseStream.WriteAsync(result);
+					sequence.Clear();
+				}
+			}
+		}
+
 		public override Task<CollatzResponse> Calculate(
 			CollatzRequest request, ServerCallContext context)
 		{
